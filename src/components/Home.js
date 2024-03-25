17220ef61409c55df3a27891d818a07d9c31bdaf
Noman Ahmad <nomanahmed1919@gmail.com>
2024-03-25 15:39:00 -0700

    Picture update

--- src/components/Home.js
+++ src/components/Home.js
@@ -16,18 +16,25 @@ const Home = () => {
 
 
                     <Header />
-                    <div className="container">
+                    <div className="container-fluid">
 
                         <div className="row">
                             <div className="col-12">
 
+
                                 <h1 className="welcome">WELCOME  TO <br></br> <span className="welcome1">BYTEBRIT</span></h1>
                                 <div className="marquee-container">
                                     <div className="marque-width">
-                                        <h1 className="marquee" >EMPOWERING BUSINESSES THROUGH EMPOWERING BUSINESSES THROUGH</h1>
+                                        {/* <h1 className="marquee" >EMPOWERING BUSINESSES THROUGH EMPOWERING BUSINESSES THROUGH</h1> */}
+                                        <h1 className="marquee" >Coming Soon!</h1>
 
                                     </div>
+
+
                                 </div>
+
+                                {/* <h1 className="soon">Coming Soon</h1> */}
+
 
                             </div>
                         </div>
