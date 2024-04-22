8a835f2530bdf371ceb1f4611d4c87d6a4939fef
Noman Ahmad <nomanahmed1919@gmail.com>
2024-04-22 06:25:58 -0700

    horizontal scrolling done

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import Header from "./Header";
-import Marquee from "./Marquee";
-import image1 from "../images/DESIGN.png"
+
 
 
 const Home = () => {
+
+
+    const [scrollPosition, setScrollPosition] = useState(0);
+
+    useEffect(() => {
+        
+        const handleScroll = () => {
+            setScrollPosition(window.scrollY);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);       
+
+   
+      
+
+
+
     return (
         <>
 
@@ -20,33 +41,74 @@ const Home = () => {
 
                         <div className="row">
                             <div className="col-12">
-
-
                                 <h1 className="welcome">WELCOME  TO <br></br> <span className="welcome1">BYTEBRIT</span></h1>
                                 <div className="marquee-container">
                                     <div className="marque-width">
                                         {/* <h1 className="marquee" >EMPOWERING BUSINESSES THROUGH EMPOWERING BUSINESSES THROUGH</h1> */}
                                         <h1 className="marquee" >Coming Soon!</h1>
 
+
                                     </div>
-
-
                                 </div>
 
                                 {/* <h1 className="soon">Coming Soon</h1> */}
-
-
                             </div>
                         </div>
-
                     </div>
-
-
-
-
-
                 </div>
+
+                <div id="section2">
+
+<div className="container">
+    <div className="row">
+        <div className="col-12">
+
+            <h1 className="h1about">ABOUT</h1>
+
+        </div>
+
+    </div>
+</div>
+
+
+
+
+
+<div className="container pb-5">
+    <div className="row">
+        <div className="col-12 work1">
+
+            <p>In the fast-paced world of digital marketing staying ahead requires not only adaptability but also a deep understanding of the unique needs of each brand. ByteBrit was founded with a vision to bridge the gap between business and their digitalpotential.Our story is one of continuous growth, innovation, and a story is one of continuous growth, innovation, and a relentless pursuit of excellence.</p>
+
+       
+        </div>
+
+    </div>
+
+</div>
+
+
+
+
+</div>
+
+
+
+
+
+<div className='para1' style={{ position: 'fixed', top: '40%', right: `${-200 - scrollPosition}px`, whiteSpace: 'nowrap' }}>
+      <h1>Work</h1>
+    </div>
+
+
+
             </div>
+
+
+
+
+         
+
 
 
         </>
