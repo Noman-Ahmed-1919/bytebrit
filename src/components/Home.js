f84e3cbb7a88d1674be68090f20edd373f875bbd
Noman Ahmad <nomanahmed1919@gmail.com>
2024-04-22 17:13:27 -0700

    work done

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,13 +3,15 @@ import Header from "./Header";
 
 
 
+
+
 const Home = () => {
 
 
     const [scrollPosition, setScrollPosition] = useState(0);
 
     useEffect(() => {
-        
+
         const handleScroll = () => {
             setScrollPosition(window.scrollY);
         };
@@ -19,19 +21,22 @@ const Home = () => {
         return () => {
             window.removeEventListener('scroll', handleScroll);
         };
-    }, []);       
+    }, []);
 
-   
-      
+    useEffect(() => {
+        console.log(scrollPosition)
 
+
+       
+    }, []);
 
 
     return (
         <>
 
-<div className="sub-sec">
+            <div className="sub-sec">
 
-            <div id="section1">
+                <div id="section1">
 
 
 
@@ -59,55 +64,50 @@ const Home = () => {
 
                 <div id="section2">
 
-<div className="container">
-    <div className="row">
-        <div className="col-12">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-12">
 
-            <h1 className="h1about">ABOUT</h1>
+                                <h1 className="h1about">ABOUT</h1>
 
-        </div>
+                            </div>
 
-    </div>
-</div>
-
-
-
-
-
-<div className="container pb-5">
-    <div className="row">
-        <div className="col-12 work1">
-
-            <p>In the fast-paced world of digital marketing staying ahead requires not only adaptability but also a deep understanding of the unique needs of each brand. ByteBrit was founded with a vision to bridge the gap between business and their digitalpotential.Our story is one of continuous growth, innovation, and a story is one of continuous growth, innovation, and a relentless pursuit of excellence.</p>
-
-       
-        </div>
-
-    </div>
-
-</div>
-
-
-
-
-</div>
+                        </div>
+                    </div>
 
 
 
 
 
-<div className='para1' style={{ position: 'fixed', top: '40%', right: `${-200 - scrollPosition}px`, whiteSpace: 'nowrap' }}>
-      <h1>Work</h1>
-    </div>
+                    <div className="container pb-5">
+                        <div className="row">
+                            <div className="col-12 work1">
+
+                                <p>In the fast-paced world of digital marketing staying ahead requires not only adaptability but also a deep understanding of the unique needs of each brand. ByteBrit was founded with a vision to bridge the gap between business and their digitalpotential.Our story is one of continuous growth, innovation, and a story is one of continuous growth, innovation, and a relentless pursuit of excellence.</p>
+
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+
+
+
+                </div>
 
 
 
             </div>
 
+            <div className='para1' style={{ position: 'fixed', top: '10%', right: `${100 + scrollPosition}px`, whiteSpace: 'nowrap' }}>
+                    <h1 style={{ transform: `translateX(-${scrollPosition}px)` }}>Work</h1>
+                </div>
 
 
 
-         
+
 
 
 
