4a04adeeda380722459ce8e1d3d9c6a9789ff326
Noman Ahmad <nomanahmed1919@gmail.com>
2024-05-01 16:53:33 -0700

    working on reviews

--- src/components/About.js
+++ src/components/About.js
@@ -15,35 +15,32 @@ const About = () =>{
 
         gsap.to(sectionRef.current, {
             
-            duration: 3, 
+            duration: 7, 
 
             scrollTrigger: {
                 trigger: sectionRef.current,
-                start: 'top center',
+                start: '',
 
                 end: 'bottom center',
                 scrub: true,
-              
+                onToggle: self => {
+                    const isCenter = self.isActive;
+                    sectionRef.current.style.color = isCenter ? '#531391' : 'white';
+                },
 
             },
             x: '52%', // Move to the center of the viewport
             ease: 'power2.inOut', // Use easing for smooth animation
-            onUpdate: self => {
-                const position = self.getVelocity() >= 0 ? 'start' : 'end'; // Check if scrolling up or down
-                const isCenter = self.progress === 0.5; // Check if the element is at the center of the viewport
-                const textColor = isCenter ? 'white' : 'purple'; // Change color to white when at the center
-
-                sectionRef.current.style.color = textColor; // Set the color of the element
-            },
+           
         });
 
 
         gsap.to(workRef.current, {
-            duration: 5, // Adjust the duration as needed
+            duration: 7, // Adjust the duration as needed
 
             scrollTrigger: {
                 trigger: workRef.current,
-                start: 'top bottom',
+                start: '',
 
                 end: 'bottom center',
 
@@ -65,11 +62,14 @@ const About = () =>{
 
 <div className="container-fluid">
     <div className="row">
-        <div className="col-12">
+        <div className="col-12" id='auto'>
 
             <h1   ref={sectionRef}  className="h1about">ABOUT</h1>
            
+           <div className='aboutwork'>
            <p  ref={workRef}  className='inthe'>In the fast-paced world of digital marketing staying ahead requires not only adaptability but also a deep understanding of the unique needs of each brand. ByteBrit was founded with a vision to bridge the gap between business and their digitalpotential.Our story is one of continuous growth, innovation, and a story is one of continuous growth, innovation, and a relentless pursuit of excellence.</p>
+
+           </div>
 
 
         </div>
---
+++ src/components/Reviews.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+
+const Reviews = () =>{
+    return(
+        <>
+        
+<div className="container">
+    <div className="row">
+        <div className="col-12">
+
+
+        </div>
+
+    </div>
+
+</div>
+
+
+        </>
+    )
+}
+export default Reviews;
