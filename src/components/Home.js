1e60277619a3436a7c3fcccc4f9afd554fedccd9
Noman Ahmad <nomanahmed1919@gmail.com>
2024-05-23 15:50:48 -0700

    home responsiveness done

--- src/components/Home.js
+++ src/components/Home.js
@@ -45,26 +45,22 @@ const Home = () => {
                             <div className="col-12">
 
                                 <div className="main-heading">
-                                <h1 className="welcome">WELCOME &nbsp;TO <br></br> <span className="welcome1">BYTEBRIT</span></h1>
+                                   
+                                   
+                                    <h1 className="welcome">WELCOME &nbsp;TO <br></br> <span className="welcome1">BYTEBRIT</span></h1>
 
-<div className="marquee-container">
+                                            <Marquee direction="left" speed={100} delay={5}>
 
-    <div className="marque-width">
+                                                {/* <h1 className="marquee" >EMPOWERING BUSINESS THROUGH TECHNOLOGY</h1> */}
 
-        <Marquee direction="left" speed={100} delay={5}>
-
-            {/* <h1 className="marquee" >EMPOWERING BUSINESS THROUGH TECHNOLOGY</h1> */}
-
-            <h1 className="marquee" >WE &nbsp; ARE &nbsp; CURRENTLY REBUILDING</h1>
+                                                <h1 className="marquee" >WE &nbsp; ARE &nbsp; CURRENTLY REBUILDING</h1>
 
 
-        </Marquee>
+                                            </Marquee>
 
 
-    </div>
-</div>
                                 </div>
-                               
+
 
                                 <p className='guide' >We guide game-changing Tech, AI & Commerce <br></br> Brands, across platforms & places,<br></br> through agile design & digital experience.
                                 </p>
